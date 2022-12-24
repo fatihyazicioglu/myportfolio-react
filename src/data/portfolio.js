792f7fda22f1410c14beb95db21de7661e453257
fatihyazicioglu <fy134388@gmail.com>
2022-12-24 15:41:16 +0100

    changed

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,35 +1,35 @@
 export default [
     {
        title: 'E-Commerce',
-       imgUrl: '/public/images/1.png',
+       imgUrl: 'src/public/images/1.png',
        stack: ['React', 'CSS', 'JavaScript','Bootstrap','MongoDB','Express-Middleware'],
        link: 'https://mern-ecommerce-musicshop.onrender.com/',
     },
     {
        title: 'Chat-App',
-       imgUrl: '/public/images/2.png',
+       imgUrl: 'src/public/images/2.png',
        stack: ['React', 'HTML', 'JavaScript','Socket.io','redux','MongoDB','Express-Middleware'],
        link: 'https://mern-chatapp.onrender.com/',
     },
     {
        title: 'music-app',
-       imgUrl: 'public/images/3.png',
+       imgUrl: 'src/public/images/3.png',
        stack: ['HTML', 'CSS', 'JavaScript','React'],
        link: 'https://fatih-music-app.vercel.app/',
     },
     {
       title: 'Gallery-Wall',
-      imgUrl: '/public/images/4.png',
+      imgUrl: 'src/public/images/4.png',
       stack: ['HTML', 'CSS'],
       link: 'https://fatihyazicioglu.github.io/Gallery-Wall/',
    },{
       title: 'To-Do List',
-      imgUrl: '/public/images/5.png',
+      imgUrl: 'src/public/images/5.png',
       stack: ['HTML', 'CSS','JavaScript'],
       link: 'https://fatihyazicioglu.github.io/To-Do-List/',
    },{
       title: 'my Portfolio',
-      imgUrl: '/public/images/6.png',
+      imgUrl: 'src/public/images/6.png',
       stack: ['HTML', 'CSS', 'JavaScript','React','Vite','TailwindCss','Getform.io'],
       link: 'https://fatihyazicioglu.github.io/myportfolio-react/',
    },
